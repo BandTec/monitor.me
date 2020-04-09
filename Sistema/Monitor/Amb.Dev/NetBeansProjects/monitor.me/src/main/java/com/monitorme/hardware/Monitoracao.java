dbe7a3ede6f937a970a421fd958c424489a7deb7
vulgosouza <gabriel.arruda@bandtec.com.br>
2020-04-09 15:32:26 -0300

    update: change local variables

--- Sistema/Monitor/Amb.Dev/NetBeansProjects/monitor.me/src/main/java/com/monitorme/hardware/Monitoracao.java
+++ Sistema/Monitor/Amb.Dev/NetBeansProjects/monitor.me/src/main/java/com/monitorme/hardware/Monitoracao.java
@@ -3,21 +3,21 @@ package com.monitorme.hardware;
 
 public class Monitoracao {
     
-    private Double utilizacaoCpu;
-    private Double velocCpu;
-    private Double processoCpu;
-    private Double dispoMemor;
-    private Double usoMemor;
-    private Double cacheMemor;
-    private Double atvDisco;
-    private Double velocDisco;
-    private Double respoDisco;
+    private Double utilizacaoCpu,
+            velocCpu,
+            processoCpu,
+            dispoMemor,
+            usoMemor,
+            cacheMemor,
+            atvDisco,
+            velocDisco,
+            respoDisco;
     
-     public Monitoracao () {
+    public Monitoracao () {
         System.out.println("");
     }
         
-     private void getCpu() {
+    private void getCpu() {
          this.processoCpu = 0.0;
          this.utilizacaoCpu= 0.0;
          this.velocCpu = 0.0;
@@ -32,4 +32,78 @@ public class Monitoracao {
          this.atvDisco = 0.0;
          this.respoDisco = 0.0;
     }
+
+    public Double getUtilizacaoCpu() {
+        return utilizacaoCpu;
+    }
+
+    public void setUtilizacaoCpu(Double utilizacaoCpu) {
+        this.utilizacaoCpu = utilizacaoCpu;
+    }
+
+    public Double getVelocCpu() {
+        return velocCpu;
+    }
+
+    public void setVelocCpu(Double velocCpu) {
+        this.velocCpu = velocCpu;
+    }
+
+    public Double getProcessoCpu() {
+        return processoCpu;
+    }
+
+    public void setProcessoCpu(Double processoCpu) {
+        this.processoCpu = processoCpu;
+    }
+
+    public Double getDispoMemor() {
+        return dispoMemor;
+    }
+
+    public void setDispoMemor(Double dispoMemor) {
+        this.dispoMemor = dispoMemor;
+    }
+
+    public Double getUsoMemor() {
+        return usoMemor;
+    }
+
+    public void setUsoMemor(Double usoMemor) {
+        this.usoMemor = usoMemor;
+    }
+
+    public Double getCacheMemor() {
+        return cacheMemor;
+    }
+
+    public void setCacheMemor(Double cacheMemor) {
+        this.cacheMemor = cacheMemor;
+    }
+
+    public Double getAtvDisco() {
+        return atvDisco;
+    }
+
+    public void setAtvDisco(Double atvDisco) {
+        this.atvDisco = atvDisco;
+    }
+
+    public Double getVelocDisco() {
+        return velocDisco;
+    }
+
+    public void setVelocDisco(Double velocDisco) {
+        this.velocDisco = velocDisco;
+    }
+
+    public Double getRespoDisco() {
+        return respoDisco;
+    }
+
+    public void setRespoDisco(Double respoDisco) {
+        this.respoDisco = respoDisco;
+    }
+    
+    
 }
