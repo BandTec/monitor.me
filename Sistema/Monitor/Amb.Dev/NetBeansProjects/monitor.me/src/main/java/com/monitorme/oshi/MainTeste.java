a13a3b030d23730b30cf4a1b98ccb2a66be339cb
vulgosouza <gabriel.arruda@bandtec.com.br>
2020-06-08 14:07:15 -0300

    update: class memoria done

---
+++ Sistema/Monitor/Amb.Dev/NetBeansProjects/monitor.me/src/main/java/com/monitorme/oshi/MainTeste.java
@@ -0,0 +1,11 @@
+
+package com.monitorme.oshi;
+
+
+public class MainTeste {
+    public static void main(String[] args) {
+        Memoria m = new Memoria();
+        
+        System.out.println(m.dadosMemoria());
+    }
+}
