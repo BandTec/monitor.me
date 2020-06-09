bb845a38ec4d1f146e70ab030857218be9cc30c2
igor amaral <54115456+igoraleite1268@users.noreply.github.com>
2020-06-09 18:29:26 -0300

    alteração do banco
    alteração do banco

--- Banco de Dados/Modelagem/1tbl_HardwareHistories.sql
+++ Banco de Dados/Modelagem/1tbl_HardwareHistories.sql
@@ -1,8 +1,8 @@
 create table tbl_HardwareHistories(
 IdHardwareHistories int primary key identity,
-MemoriaRamReg varchar(45),
-GPUReg varchar(45),
-DiscoRigidoReg varchar(45),
---Id_IdHardware int
---foreign key (Id_IdHardware) references tbl_Hardware(IdHardware)
+Data date,
+OshiStatus varchar(255),
+GPUStatus varchar(255),
+Id_IdHardware int
+foreign key (Id_IdHardware) references tbl_Hardware(IdHardware)
 )
