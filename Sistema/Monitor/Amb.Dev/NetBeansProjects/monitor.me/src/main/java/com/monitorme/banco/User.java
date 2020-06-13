a7b47aaf35388edd2a983decfd4b389cda2ad750
Igor <igor.leite@bandtec.com.br>
2020-06-13 19:05:19 -0300

    Alterações no pegar informações de Login

--- Sistema/Monitor/Amb.Dev/NetBeansProjects/monitor.me/src/main/java/com/monitorme/banco/User.java
+++ Sistema/Monitor/Amb.Dev/NetBeansProjects/monitor.me/src/main/java/com/monitorme/banco/User.java
@@ -1,5 +1,6 @@
 package com.monitorme.banco;
 
+import com.monitorme.tela.TelaLogin;
 import java.util.List;
 import javax.swing.JOptionPane;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,28 +15,30 @@ public class User {
 
     public User(String email, String senha) {
         try {
-
+               
+            TelaLogin login = new TelaLogin();
             ConexaoBanco dadosConexao = new ConexaoBanco();
             JdbcTemplate jdbcTemplate = new JdbcTemplate(dadosConexao.getDataSource());
-            List lista = jdbcTemplate.queryForList("select login,email,senha,fkComputador from usuario"
-                    + " where login='" + email + "'and senha='" + senha + "'or email='" + email + "'and senha='" + senha + "'");
-            
+            List lista = jdbcTemplate.queryForList("select Email,Senha from tblUsers"
+                    + " where Email='" + email + "'and Senha='" + senha + "'or Email='" + email + "'and Senha='" + senha + "'");
+            List ID = jdbcTemplate.queryForList("select IdUsers from tblUsers"
+            + " where Email='" + email + "'and Senha='" + senha + "'or Email='" + email + "'and Senha='" + senha + "'");
             if (lista.isEmpty() == false) {
                 String split = lista.toString();
                 String[] split2 = split.split("=", 5);
                 String[] comp = split2[4].split("}");
                 System.out.println(comp[0]);
                 UsuarioLogado = Integer.valueOf(comp[0]);
-                //Gui, coloca no Log a info abaixo
-//                logger.info("Logado com sucesso");
+               
             } else {
-                //Gui, coloca no Log a info abaixo
-//                logger.info("Usuário não encontrado");
+              
                 JOptionPane.showMessageDialog(null, "Usuário ou Senha inválidos!", "Login inválido", JOptionPane.ERROR_MESSAGE);
             }
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
-
+        
     }
+   
+            
 }
